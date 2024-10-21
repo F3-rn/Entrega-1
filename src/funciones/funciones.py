6fb1a343d6af80a1ee5919c9600d9b7edd5cb068
F3-rn <ferdel3612@gmail.com>
2024-10-21 19:38:40 +0200

    Trabajo hecho

--- src/funciones/funciones.py
+++ src/funciones/funciones.py
@@ -1,1 +1,68 @@
+#EJERCICIO 1
+from math import factorial
 
+def productorio(n:int,k:int)->int:
+    i:int=0
+    p:int=1
+    if (n <= k):
+        raise Exception("El primer número tiene que ser mayor que el segundo")
+    else:
+        while k>=i:
+            p = (n-i+1)*p
+            i = i+1
+    return(p)
+
+
+
+#EJERCICIO 2
+def secuencia(a:int, r:int, k:int):
+    i:int=1
+    producto = list()
+    m:int=1
+    while i<= k:
+        a= a*r**(i-1)
+        i = i+1
+        producto.append(a)
+    for n in producto:
+        m= m*n
+    return(m)
+
+
+#EJERCICIO 3
+def combinatorio(n:int,k:int):
+    if (n < k):
+        raise Exception("El primer número tiene que ser mayor o igual que el segundo")
+    else:
+        return(factorial(n)/(factorial(k)*factorial(n - k)))
+        
+
+#EJERCICIO 4
+def numeroespecial(n:int,k:int):
+    i=0
+    parte2=0
+    if (n < k):
+        raise Exception("El primer número tiene que ser mayor o igual que el segundo")
+    else:
+        parte1 = 1/factorial(k)
+        while i<k:
+            parte2 = parte2 + combinatorio(k + 1, i + 1)*(-1)**i*(k - i)**n
+            i = i + 1
+        return(parte1*parte2)
+
+
+
+#EJERCICIO 5
+from typing import Callable
+def newton(f:Callable[[float],float],d:Callable[[float],float],a:float,e:float)->float:
+    while abs(f(a))>e:
+        a = a - f(a)/d(a)
+    return(a)
+
+
+
+if __name__ == '__main__':
+    print(productorio(4, 2))
+    print(secuencia(3, 5, 2))
+    print(combinatorio(4, 2))
+    print(numeroespecial(4, 2))
+    print(newton(lambda x: 2*x**2, lambda x: 4*x,3,0.001))
--- src/lecturas/lecturas.py
+++ src/lecturas/lecturas.py
@@ -1,1 +1,59 @@
+#EJERCICIO 6
+from _csv import reader
 
+def contador(fichero:str, sep:str,cad:str)-> int:
+    palabra:int = 0
+    with open(fichero) as f:
+        lector= reader(f, delimiter=sep)
+        for n in lector:
+            for y in n:
+                if y == cad:
+                    palabra = palabra + 1
+    return(palabra)
+
+
+
+#EJERCICIO 7
+def lineas_con_palabra(fichero:str, palabra:str) -> list[str]:
+    lista = []
+    with open(fichero) as f:
+        for linea in f:
+            if palabra in linea:
+                    lista.append(linea.strip())
+    return(lista)
+
+
+
+#EJERCICIO 8
+def palabras_fichero(fichero:str) -> list[str]:
+    lista = []
+    with open(fichero) as f:
+        lector= reader(f, delimiter=' ')
+        for n in lector:
+            for y in n:
+                if (y not in lista) and (y != ''):
+                    lista.append(y)
+    return(lista)
+
+
+
+#EJERCICIO 9
+def longitud_promedio_lineas(file_path: str, sep:str) -> float:
+    lista = []
+    i:int = 0
+    with open(file_path) as f:
+        lector= reader((linea.strip() for linea in f), delimiter=sep)
+        for n in lector:
+            i = 0
+            for y in n:
+                i= i + 1
+            lista.append(i)
+    final= sum(lista)/len(lista)
+    return(final)
+
+
+if __name__ == '__main__':
+    print(contador('../../resources/lin_quijote.txt',' ','Quijote')) #La función es sensible a mayúsculas y minúsculas
+    print(lineas_con_palabra('../../resources/lin_quijote.txt', 'QUIJOTE')) #La función es sensible a mayúsculas y minúsculas
+    print(palabras_fichero('../../resources/archivo_palabras.txt'))
+    print(longitud_promedio_lineas('../../resources/palabras_random.csv', ','))
